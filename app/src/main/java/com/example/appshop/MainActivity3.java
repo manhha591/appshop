d81442808e1a2106fb8935a6be04629ce2d33f35
manhha591 <manhha2k1@gmail.com>
2022-05-12 16:58:01 +0700

    $ git push origin master

---
+++ app/src/main/java/com/example/appshop/MainActivity3.java
@@ -0,0 +1,15 @@
+package com.example.appshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+}
